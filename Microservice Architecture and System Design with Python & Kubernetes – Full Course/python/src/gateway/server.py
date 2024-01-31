6ce0bad8abf46f36425a947952cf380ec2b129b3
zteffano <48279765+zteffano@users.noreply.github.com>
2024-01-31 08:36:08 +0100

    opsæt af notification service + små fixes - Project Done & Testet!

--- Microservice Architecture and System Design with Python & Kubernetes – Full Course/python/src/gateway/server.py
+++ Microservice Architecture and System Design with Python & Kubernetes – Full Course/python/src/gateway/server.py
@@ -2,19 +2,30 @@ import os
 import gridfs
 import pika
 import json
-from flask import Flask, request
+from flask import Flask, request, send_file
 from flask_pymongo import PyMongo
 from auth import validate
 from auth_svc import access
 
 from storage import util
+from bson.objectid import ObjectId
 
 server = Flask(__name__)
 server.config["MONGO_URI"] = "mongodb://host.minikube.internal:27017/videos"
 
-mongo = PyMongo(server)
 
-fs = gridfs.GridFS(mongo.db)
+mongo_video = PyMongo(
+    server,
+    uri="mongodb://host.minikube.internal:27017/videos"
+    )
+
+mongo_mp3 = PyMongo(
+    server,
+    uri="mongodb://host.minikube.internal:27017/mp3s"
+    ) 
+
+fs_videos = gridfs.GridFS(mongo_video.db)
+fs_mp3s = gridfs.GridFS(mongo_mp3.db)
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
     "rabbitmq"))  # Making the connection with rabbitmq
@@ -35,16 +46,19 @@ def login():
 def upload():
     access, err = validate.token(request)
 
+    if err:
+        return err
+
     access = json.loads(access)
 
     if access["admin"]:
-        print(f"--> Request Files: {request.files}")
+
         if len(request.files) > 1 or len(request.files) < 1:
             return "exactly 1 file required", 400
         
 
         for _, f in request.files.items():
-            err = util.upload(f,fs,channel, access)
+            err = util.upload(f,fs_videos ,channel, access)
 
             if err:
                 return err
@@ -56,7 +70,27 @@ def upload():
 
 @server.route("/download", methods=["GET"])
 def download():
-    pass
+    access, err = validate.token(request)
+
+    if err:
+        return err
+
+    access = json.loads(access)
+
+    if access["admin"]:
+        fid_string = request.args.get("fid")
+
+        if not fid_string:
+            return "Fid is required", 400
+        
+        try:
+            out = fs_mp3s.get(ObjectId(fid_string))
+            return send_file(out, download_name=f'{fid_string}.mp3')
+        except Exception as err:
+            print(err)
+            return "Internal server error", 500
+
+    return "Not authorized", 401
 
 
 if __name__ == "__main__":
